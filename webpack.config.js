ac1bbbcb97c8f0cf7ab827cb3f35d897b8d8b36c
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-01-15 22:30:05 +0800

    Added loaders for CSS and JSHint

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,73 @@
+const merge = require('webpack-merge');
 const path = require('path');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-  // Entry accepts a path or an object of entries.
-  // The build chapter contains an example of the latter.
-  entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'Kanban app'
-    })
-  ]
+const common = {
+    // Entry accepts a path or an object of entries.
+    // The build chapter contains an example of the latter.
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js?$/,
+                loaders: ['jshint'],
+                // define an include so we check just the files we need
+                include: PATHS.app
+            }
+        ],
+        loaders: [
+            {
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            }
+        ]
+        // postLoaders section for stuff like code coverage checking
+    },
+
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'Kanban app'
+        })
+    ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+
+            // Parse host and port from env so this is easy to customize.
+            host: process.env.HOST,
+            port: process.env.PORT || 3000
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        devtool: 'eval-source-map'
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {})
+}
