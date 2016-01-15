d6b62aa68f08471309bc1e3968a1df293985e439
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-01-15 20:53:11 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
