f149da94a2831557d037b5d4f109733b74226155
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-01-17 22:03:47 +0800

    wtf react is sick. Simple implementation of editable text

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,57 @@
 import React from 'react';
 
-export default ({task}) => <div>{task}</div>;
+class Note extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false
+        };
+    }
+
+    render() {
+        if (this.state.editing) {
+            return (
+                <input type="text"
+                       autofocus={true}
+                       placeholder={this.props.task}
+                       onBlur={this.finishEdit}
+                       onKeyPress={this.onKeyPress}
+                />
+            )
+        }
+
+        return <div onClick={this.edit}>{this.props.task}</div>
+    }
+
+    onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            this.finishEdit(e)
+        }
+    };
+
+    edit = () => {
+        this.setState({
+            editing: true
+        })
+    };
+
+    finishEdit = (e) => {
+        this.props.onEdit(e.target.value)
+
+        this.setState({
+            editing: false
+        })
+    };
+}
+
+Note.propTypes = {
+    onEdit: React.PropTypes.func
+};
+
+Note.defaultProps = {
+    onEdit: (body)=> {
+    }
+};
+
+export default Note
